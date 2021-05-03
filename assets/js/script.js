6c235375564d0442727e027e39670dc3bdeecaa4
Drew Selden <seldend1@gmail.com>
2021-05-03 15:50:15 -0400

    added time display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,32 @@
 var currentDayEl = $('#currentDay');
+var saveButton = $('.saveBtn')
 
-
+// get current time and display it
 function displayTime() {
-    var rightNow = moment().format('dddd, MMMM Do YYYY [at] h:mm:ss a');
+    var rightNow = moment().format('dddd, MMMM Do YYYY [at] h:mm:ss A');
     currentDayEl.text(rightNow);
-    
-  }
+}
+
+var currentTime = moment().format("HH")
+console.log(currentTime);
+
+function setClassStatus() {
+    if (row.val < currentTime) {
+        $(".row").addClass("past");
+    } else if (row.val === currentTime ) {
+       $(".row").addClass("present");
+    } else if (row.val > currentTime) {
+        $(".row").addClass("future")
+    }
+}
+saveButton.on('click', function(event) {
+    event.preventDefault();
+
+})
+
+
+
+
+
+
   setInterval(displayTime, 1000);
